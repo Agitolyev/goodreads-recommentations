c66bcda5a9a5b07d8e796d26758d8d897d4be847
Alex <aagitolyev@kaaiot.io>
2019-08-31 19:31:54 +0300

    refactor: split functions, add comments, minor refactoring

--- src/goodreads/gr_api.clj
+++ src/goodreads/gr_api.clj
@@ -24,11 +24,13 @@
       :body
       bs/to-string))
 
+(defn str->xml [input]
+  (let [input-xml (java.io.StringReader. input)]
+    (xml/parse input-xml)))
+
 (defn http-get-xml->json [url]
-  (def response (http-get->str url))
-  (def parsed-xml (let [input-xml (java.io.StringReader. response)]
-                    (xml/parse input-xml)))
-  (xml->json parsed-xml))
+  (let [response (http-get->str url)]
+    (xml->json (str->xml response))))
 
 (defn retrieve-shelf-books-info [shelf dev-key target-key]
   "Retrieves books in specified self and returns books info by specified key. Info source - goodreads"
@@ -38,8 +40,9 @@
               (:reviews (first
                           (filter (fn [el] (contains? el :reviews))
                                   (:GoodreadsResponse resp)))))))
-  (def read-list-url (str "https://www.goodreads.com/review/list/78268448.xml?&v=2&shelf=" shelf "&key=" dev-key))
-  (filter (comp not nil?) (map target-key (extract-books-info (http-get-xml->json read-list-url)))))
+
+  (let [read-list-url (str "https://www.goodreads.com/review/list/78268448.xml?&v=2&shelf=" shelf "&key=" dev-key)]
+    (filter (comp not nil?) (map target-key (extract-books-info (http-get-xml->json read-list-url))))))
 
 (defn retrieve-shelf-books-ids [shelf dev-key] (retrieve-shelf-books-info shelf dev-key :id))
 (defn retrieve-shelf-books-isbn [shelf dev-key] (retrieve-shelf-books-info shelf dev-key :isbn))
@@ -50,7 +53,7 @@
 
 (defn retrieve-similar-books [read-books-ids dev-key]
   "Retrieves similar books by given read books. Info source - goodreads"
-  (defn book-info-url [id] (str "https://www.goodreads.com/book/show/" id ".xml?key=" dev-key))
+  (defn book-info-url [id, dev-key] (str "https://www.goodreads.com/book/show/" id ".xml?key=" dev-key))
   (defn parse-books-info [response]
     (map (fn [book-info] (apply merge (:book book-info)))
          (:similar_books (first (filter
@@ -58,14 +61,14 @@
                                   (:book (first (filter
                                                   (fn [el] (contains? el :book))
                                                   (:GoodreadsResponse response)))))))))
-  (set (mapcat (fn [book-id] (parse-books-info (http-get-xml->json (book-info-url book-id)))) read-books-ids)))
+  (set (mapcat (fn [book-id] (parse-books-info (http-get-xml->json (book-info-url book-id dev-key)))) read-books-ids)))
 
 (defn retrieve-similar-books-isbn [read-books dev-key] (filter (comp not nil?) (map :isbn (retrieve-similar-books read-books dev-key))))
 
 (defn retrieve-book-info-by-isbn [book-isbn]
-  (def isbn-key (str "ISBN:" book-isbn))
-  (def book-info-url (str "http://openlibrary.org/api/books?bibkeys=" isbn-key "&format=json&jscmd=data"))
-  (get (json/read-str (http-get->str book-info-url)) isbn-key))
+  (let [isbn-key (str "ISBN:" book-isbn)
+        book-info-url (str "http://openlibrary.org/api/books?bibkeys=" isbn-key "&format=json&jscmd=data")]
+    (get (json/read-str (http-get->str book-info-url)) isbn-key)))
 
 (defn book-info->book [book-info]
   (Book.
@@ -76,36 +79,38 @@
 
 (defn retrieve-book-subjects [book-isbn]
   "Retrieves book subjects by given book ISBN. Info source - openlibrary"
-  (def raw-subjects (get (retrieve-book-info-by-isbn book-isbn) "subjects"))
-  (map (fn [subj] {:name (get subj "name") :url (get subj "url")}) raw-subjects))
+  (let [raw-subjects (get (retrieve-book-info-by-isbn book-isbn) "subjects")]
+    (map (fn [subj] {:name (get subj "name") :url (get subj "url")}) raw-subjects)))
 
 (defn retrieve-target-subjects [books-read, dev-key]
   "Retrieves books similar to that you`ve already read and finds subjects.
    Returns top 25 (empirical value) most frequent subjects weighted by occ num. Info source - openlibrary
    books-read - List of Goodreads books ids"
-  (def subjects (mapcat retrieve-book-subjects (retrieve-similar-books-isbn books-read dev-key)))
-  (def top-subj (take 25 (sort-by (fn [el] (- (get el 1))) (apply vector (frequencies subjects)))))
-  (map (fn [el] (assoc (first el) :weight (get el 1))) top-subj))
+  (let [subjects (mapcat retrieve-book-subjects (retrieve-similar-books-isbn books-read dev-key))]
+    (let [top-subjects (take 25 (sort-by (fn [el] (- (second el))) (apply vector (frequencies subjects))))]
+      (map (fn [el] (assoc (first el) :weight (second el))) top-subjects))))
 
 (defn retrieve-books-by-subject [subject]
   (map
-    (fn [raw-book]
-      (Book.
-        (get raw-book "title")
-        (get raw-book "edition_count")
-        (get raw-book "subject")
-        (openlib-key->link (get raw-book "key"))))
+    book-info->book
     (get (json/read-str (http-get->str (str (:url subject) ".json?details=true"))) "works")))
 
 (defn book->features [target-subjects, book]
-  (def book-subjects (set (:subject book)))
-  (def subj-vector (apply merge (map
-                                  (fn [subject] (let [s-name (:name subject)]
-                                                  (hash-map s-name (if (contains? book-subjects s-name) (:weight subject) 0))))
-                                  target-subjects)))
+  (defn book-subj->feature-map [subj-set, target-subj]
+    "Returns book feature map by book subjects set and given weighted target subjects."
+    (apply merge (map
+                   (fn [subject] (let [s-name (:name subject)]
+                                   (hash-map s-name (if (contains? subj-set s-name) (:weight subject) 0))))
+                   target-subj)))
 
-  (def sorted-subjects (sort (map :name target-subjects)))
-  (hash-map :title (:title book)
-            :link (:link book)
-            :edition_count (:edition_count book)
-            :subject_vector (map (partial get subj-vector) sorted-subjects)))
+  (defn feature-map->ordered-vector [feature-map, ordered-features]
+    "Returns feature vectored ordered by ordered-features constructed from given feature-map"
+    (map (partial get feature-map) ordered-features))
+
+  (let [subj-set (set (:subject book))]
+    (let [feature-map (book-subj->feature-map subj-set target-subjects)
+          ordered-subjects (sort (map :name target-subjects))]
+      (hash-map :title (:title book)
+                :link (:link book)
+                :edition_count (:edition_count book)
+                :subject_vector (feature-map->ordered-vector feature-map ordered-subjects)))))
